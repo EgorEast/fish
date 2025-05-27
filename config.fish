aaf84003f6ee0187fc74575383a26fa869c8f7ec
egoreast <egoreast.true@gmail.com>
2025-05-27 11:06:37 +0300

    add shortcut for yazi

--- config.fish
+++ config.fish
@@ -31,3 +31,12 @@ if command -q fastfetch && ! string match -q -- "*yakuake*" (pstree -s -p $fish_
 end
 
 set -Ux EDITOR nvim
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
