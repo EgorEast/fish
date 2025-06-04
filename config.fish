00e4ab10e070750974422115428314e0c3cff495
egoreast <egoreast.true@gmail.com>
2025-06-04 21:43:56 +0300

    add piscec plugin

--- config.fish
+++ config.fish
@@ -36,6 +36,7 @@ set -U __done_min_cmd_duration 5000 # default: 5000 ms
 set -U __done_exclude '^git (?!push|pull|fetch)' # default: all git commands, except push and pull. accepts a regex.
 set -U __done_notify_sound 1
 set -U __done_allow_nongraphical 1
+set -U pisces_only_insert_at_eol 1
 
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
